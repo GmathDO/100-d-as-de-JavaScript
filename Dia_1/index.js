118f2bd9aaa13ad02cfea0ef2b643d2ef95b7b97
unknown <gerardo_duarte@live.com>
2024-04-11 21:08:31 -0300

    Dia 1

---
+++ Dia_1/index.js
@@ -0,0 +1,8 @@
+/* https://developer.mozilla.org/es/docs/Web/JavaScript */
+
+// https://developer.mozilla.org/es/docs/Web/JavaScript //
+
+let variable;
+const constante = 0;
+
+console.log("¡Hola JavaScript!")
