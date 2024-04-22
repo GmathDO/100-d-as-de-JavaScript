a897e9300787e0ff8482b14ff27a8dc754c33bc6
unknown <gerardo_duarte@live.com>
2024-04-21 23:04:39 -0300

    Reedición

--- Dia_7/index.js
+++ Dia_7/index.js
@@ -1,4 +1,4 @@
-function areaPoligono(poligono,base,altura){
+/*function areaPoligono(poligono,base,altura){
     if (poligono.toLowerCase() === "triangulo" || poligono.toLowerCase() === "triángulo"){
         return (base*altura)/2
     } else if(poligono.toLowerCase() === "cuadrado"){
@@ -6,4 +6,40 @@ function areaPoligono(poligono,base,altura){
     } else if(poligono.toLowerCase() === "rectangulo" || poligono.toLowerCase() === "rectángulo"){
         return base*altura;
     }
+}*/
+
+//Reedicion del codigo
+class Poligono{
+    constructor(){}
+    area(){}
+}
+
+class Triángulo extends Poligono{
+    constructor(){}
+    static area(base, altura){
+        this.base = base;
+        this.altura = altura;
+        return (this.base*this.altura)/2
+    }
+}
+
+class Cuadrado extends Poligono{
+    constructor(){}
+    static area(lado){
+        this.lado = lado;
+        return this.lado*this.lado;
+    }
+}
+
+class Rectángulo extends Poligono{
+    constructor(){}
+    static area(base, altura){
+        this.base = base;
+        this.altura = base;
+        return base*altura;
+    }
+}
+
+function areaPoligono(poligono){
+    return poligono
 }
