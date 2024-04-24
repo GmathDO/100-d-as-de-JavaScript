9e2cfa6fcb83742b4b0826906798c01fbd988045
unknown <gerardo_duarte@live.com>
2024-04-24 00:32:26 -0300

    Reedición

--- Dia_11/index.js
+++ Dia_11/index.js
@@ -3,11 +3,14 @@ function decimalBinario(decimal){
     let resto = parseInt(decimal%2)
     let resultado = []
     resultado.push(resto)
-    while(cociente > 2){
+    while(cociente != 1){
+        console.log("resto",resto)
         resto = parseInt(cociente%2);
         resultado.unshift(resto)
+        console.log("cociente:",cociente)
         cociente = parseInt(cociente/2);
     }
+    console.log("cociente",cociente)
     resultado.unshift(cociente)
     resultado = parseInt(resultado.join(""))
     console.log(resultado)
